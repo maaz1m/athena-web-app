bba75d2ad631e03e69ee07d9fb2def8b43af7d19
Mohammad Maaz <mohammadmaaz1997@gmail.com>
2019-04-06 21:00:11 +0500

    heroku deploy

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const io = require('socket.io')(server);
 const path = require('path');
-
 const app = express()
 const server = require('http').createServer(app)
+const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 8080;
 
