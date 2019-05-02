3c8360e5212919341b7aa6769ccbb6d5a5537bfe
unknown <usmansagemode@gmail.com>
2019-05-02 22:41:33 +0500

    'homepagepicture'

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,6 +9,17 @@ import Notifications from '../components/Notifications'
 import Calendar from '../components/Calendar'
 import firebase from '../firebase'
 
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+import lums from './lums.jpg';
+import iba from './iba.jpg';
+import UCP from './UCP.jpg';
+
+
 const styles = theme => ({
   root: {
     width: '30%',
@@ -29,8 +40,54 @@ const styles = theme => ({
   greeting:{
     marginTop: '5%',
     marginLeft: '35%'
-  }
+  },
+  roottt: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+    alignItems: 'baseline',
+    justify: 'space-evenly',
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+     color: theme.palette.primary.light,
+    //  'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
 });
+
+
+
+
+
+const tileData = [
+{
+  img: lums,
+  title: 'LUMS',
+  
+},
+	 {
+  img: UCP,
+  title: 'UCP',
+
+},
+ {
+  img: iba,
+  title: 'IBA',
+},
+
+];
+
+
 
 
 class HomePage extends React.Component{
@@ -55,7 +112,37 @@ class HomePage extends React.Component{
 						</div>
 					</Grid>
 				</Grid>
+				<div  className={classes.roottt} >
+			<div>	
+				<Typography component="h2" variant="h3">
+	        			Partnered Universities
+	      		</Typography>
+	      	</div>
+	      <GridList className={classes.gridList} rows={2.5} cols={2.5}>
+
+	        {tileData.map(tile => (
+	          <GridListTile key={tile.img}>
+	            <img src={tile.img} alt={tile.title} />
+	            <GridListTileBar
+	              title={tile.title}
+	              classes={{
+	                root: classes.titleBar,
+	                title: classes.title,
+	              }}
+	              actionIcon={
+	                <IconButton>
+	                  <StarBorderIcon className={classes.title} />
+	                </IconButton>
+	              }
+	            />
+	          </GridListTile>
+	        ))}
+	      </GridList>
+    	</div>
 			</div>
+		
+		
+    	
 		)
 	}
 }
@@ -65,6 +152,7 @@ class HomePage extends React.Component{
 			<Calendar/>
 		</div>
 </Grid>*/
+
 HomePage.propTypes = {
   classes: PropTypes.object.isRequired,
 };
