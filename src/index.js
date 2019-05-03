8c0799b10ed1ad1cceb7ba0db8f1d1f7f0084a62
unknown <usmansagemode@gmail.com>
2019-05-03 22:05:16 +0500

    'indexURL'

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import * as router from './router'
 import * as serviceWorker from './serviceWorker';
 import socket from './socket'
 import 'typeface-roboto'
+import lums from './lums.jpg';
 
-document.body.style = 'background: #d4ebf2;';
+document.body.style = 'background: url(https://www.booksoftitans.com/assets/uploads/2017/11/all-suggested-books-tim-ferriss-podcast.jpg)';
 // document.body.style = `background-image: ${bg};`;
 
 const root = document.getElementById('root');
 
-router.renderLoginPage()
+router.renderHomePage()
 // socket.on('HomePage', ()=>{
 // 	router.renderHomePage()
 // })
