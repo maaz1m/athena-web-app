79e99bdb2ebd06ec26a3ddddcb9511fd3f2dd942
Mohammad Maaz <mohammadmaaz1997@gmail.com>
2019-05-15 23:51:29 +0500

    Added quiz terminator

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -20,7 +20,7 @@ const styles = theme => ({
 
 
 var questions = [
-	{'ques': 'I especially love working with children.', 'options': ['Yes', 'No', 'Maybe', 'lol', 'not maybe']},
+	{'ques': 'I especially love working with children.', 'options': ['Yes', 'No'], 'cb': ['Engineering', 'Medicine']},
 	{'ques': 'I am interested in law, debate, government, and politics.', 'options': ['Yes', 'No']},
 	{'ques': 'I have a strong interest and ability in visual art.', 'options': ['Yes', 'No']},
 	{'ques': 'I am always reading a book or writing my own stories.', 'options': ['Yes', 'No']},
@@ -30,23 +30,23 @@ var questions = [
 	{'ques': 'I am interested in technology and learning how computers work.', 'options': ['Yes', 'No']},
 	{'ques': 'I enjoy learning about different parts of the world.', 'options': ['Yes', 'No']},
 	{'ques': 'I enjoy working with people, and I have strong verbal and written communication skills.', 'options': ['Yes', 'No']},
-	{'ques': 'I enjoy learning about how the human body and natural world work.', 'options': ['Yes', 'No']},
-	{'ques': 'I need the freedom to be creative and express myself.', 'options': ['Yes', 'No']},
-	{'ques': 'I am interested in conflict resolution, criminal justice or mediation.', 'options': ['Yes', 'No']},
-	{'ques': 'I am very ambitious, persuasive, and love coming up with my own ideas.', 'options': ['Yes', 'No']},
-	{'ques': 'I like to work with my hands and be outdoors.', 'options': ['Yes', 'No']},
-	{'ques': 'I like math and figuring out how things work.', 'options': ['Yes', 'No']},
-	{'ques': 'I enjoy learning about languages.', 'options': ['Yes', 'No']},
-	{'ques': 'I am interested in science and enjoy helping people.', 'options': ['Yes', 'No']},
-	{'ques': 'I enjoy performing as an actor, or working behind the scenes on theatrical productions.', 'options': ['Yes', 'No']},
-	{'ques': 'I am good with numbers and am detail-oriented.', 'options': ['Yes', 'No']},
-	{'ques': 'I like to sing and/or play musical instruments.', 'options': ['Yes', 'No']},
-	{'ques': 'I want to work with people on the margins of society or who have been oppressed.', 'options': ['Yes', 'No']},
-	{'ques': 'I am interested in graphic and/or Web design.', 'options': ['Yes', 'No']},
-	{'ques': 'I like to offer advice to others.', 'options': ['Yes', 'No']},
-	{'ques': 'I am interested in intellectual ideas, including those that are philosophical or religious.', 'options': ['Yes', 'No']},
-	{'ques': 'I am interested in filmmaking and media production.', 'options': ['Yes', 'No']},
-	{'ques': 'I love sports', 'options': ['Yes', 'No']}
+	// {'ques': 'I enjoy learning about how the human body and natural world work.', 'options': ['Yes', 'No']},
+	// {'ques': 'I need the freedom to be creative and express myself.', 'options': ['Yes', 'No']},
+	// {'ques': 'I am interested in conflict resolution, criminal justice or mediation.', 'options': ['Yes', 'No']},
+	// {'ques': 'I am very ambitious, persuasive, and love coming up with my own ideas.', 'options': ['Yes', 'No']},
+	// {'ques': 'I like to work with my hands and be outdoors.', 'options': ['Yes', 'No']},
+	// {'ques': 'I like math and figuring out how things work.', 'options': ['Yes', 'No']},
+	// {'ques': 'I enjoy learning about languages.', 'options': ['Yes', 'No']},
+	// {'ques': 'I am interested in science and enjoy helping people.', 'options': ['Yes', 'No']},
+	// {'ques': 'I enjoy performing as an actor, or working behind the scenes on theatrical productions.', 'options': ['Yes', 'No']},
+	// {'ques': 'I am good with numbers and am detail-oriented.', 'options': ['Yes', 'No']},
+	// {'ques': 'I like to sing and/or play musical instruments.', 'options': ['Yes', 'No']},
+	// {'ques': 'I want to work with people on the margins of society or who have been oppressed.', 'options': ['Yes', 'No']},
+	// {'ques': 'I am interested in graphic and/or Web design.', 'options': ['Yes', 'No']},
+	// {'ques': 'I like to offer advice to others.', 'options': ['Yes', 'No']},
+	// {'ques': 'I am interested in intellectual ideas, including those that are philosophical or religious.', 'options': ['Yes', 'No']},
+	// {'ques': 'I am interested in filmmaking and media production.', 'options': ['Yes', 'No']},
+	// {'ques': 'I love sports', 'options': ['Yes', 'No']}
 ]
 
 const RadarMap = (props) => {
@@ -102,10 +102,16 @@ class QuizPage extends React.Component{
 					<Grid item = {true} xs = {6}>
 						<Question question = {this.state.question} change = {(selected)=>{
 							if(selected){
-								if(this.index >= questions.length){
-									alert('Athena has chosen ' + getHighest(this.state.majorPts) + ' as an appropriate major for you')
-									router.renderHomePage()
-									return
+								if(this.index === questions.length-1){
+									var maxPts = 0
+									var maxMjr = ''
+									for (var i = 0; i < this.state.majorPts.length; i++) {
+										if(this.state.majorPts[i].pts > maxPts){
+											maxPts = this.state.majorPts[i].pts
+											maxMjr = this.state.majorPts[i].name
+										}
+									}
+									router.renderSuccessMsg(maxMjr + ' is the choice for you !')
 								} else{
 									this.index+=1
 									var copyOf = this.state.majorPts 
