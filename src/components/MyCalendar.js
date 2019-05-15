134052c79d2cc091f3aac292cd4a35aa36ae2b4d
unknown <usmansagemode@gmail.com>
2019-05-15 16:43:43 +0500

    'deadline'

--- src/components/MyCalendar.js
+++ src/components/MyCalendar.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Calendar from 'react-calendar'
+import Typography from '@material-ui/core/Typography'	;
 
 class MyCalendar extends React.Component{
 	constructor(props){
 		super(props)
+		console.log(new Date())
+		this.date = new Date(2019, 4, 22)
 	}
 
 
@@ -12,8 +15,11 @@ class MyCalendar extends React.Component{
 
 			<div>
 				<Calendar
-				value = {new Date()}
+				value = {this.date}
 				/>
+			<Typography component="h2" variant="h6">
+        		SAT Deadline: {(this.date.getDate()).toString() + '/ ' + (this.date.getMonth() +1).toString() +' / ' + (this.date.getFullYear()).toString()}
+      		</Typography>
 			</div>
 		)
 
